1c8ba2f32a483495573201b9ff0a61883686dd24
claylolx <145122199+claylolx@users.noreply.github.com>
2024-05-16 08:41:06 +0200

    Añadido Unidades y H12

--- app.py
+++ app.py
@@ -15,22 +15,22 @@ def index():
         barrio = request.form['barrio']
         dia_mes = request.form['dia_mes']
         
-        # Validar que el campo "dia_mes" no este vacio
+        # Validar que el campo "dia_mes" no esté vacío
         if not dia_mes:
             return render_template('error.html', message="Debe proporcionar un día del mes.")
         
-        # Convertir el dia del mes en numero entero
+        # Convertir el día del mes a entero
         try:
             dia_mes = int(dia_mes)
         except ValueError:
             return render_template('error.html', message="El día del mes debe ser un número entero.")
         
-        # Obtener el codigo de la estacion para el barrio seleccionado
+        # Obtener el código de la estación de monitoreo para el barrio seleccionado
         estacion_info = db.Estaciones.find_one({'Nom_barri': barrio})
         if estacion_info:
             estacion_codigo = estacion_info['Estacio']
             
-            # Consultar la base de datos para obtener los datos de contaminacion
+            # Consultar la base de datos para obtener los datos de contaminación
             collection = db.CalidadAire
             contaminacion_data = collection.find_one({'ESTACIO': estacion_codigo, 'DIA': dia_mes})
             
@@ -40,17 +40,24 @@ def index():
                 contaminante_info = db.Contaminantes.find_one({'Codi_Contaminant': codi_contaminant})
                 if contaminante_info:
                     desc_contaminant = contaminante_info['Desc_Contaminant']
+                    unitats = contaminante_info.get('Unitats', 'Desconocidas')
                 else:
                     desc_contaminant = "Tipo de contaminante desconocido"
+                    unitats = "Desconocidas"
                 
                 # Pasar el nombre del barrio seleccionado y el tipo de contaminante a la plantilla resultado.html
-                return render_template('resultado.html', barrio=barrio, contaminacion_data=contaminacion_data, desc_contaminant=desc_contaminant)
+                return render_template('resultado.html', 
+                                       barrio=barrio, 
+                                       contaminacion_data=contaminacion_data, 
+                                       desc_contaminant=desc_contaminant, 
+                                       unitats=unitats,
+                                       h12=contaminacion_data['H12'])
         
-        # Si no se encontraron datos de contaminacion para el barrio y dia seleccionados
+        # Si no se encontraron datos de contaminación para el barrio y día seleccionados
         return render_template('error.html', message="No se encontraron datos de contaminación para el barrio y día seleccionados.")
         
     else:
-        # Obtener la lista de nombres de barrio desde la coleccion Estaciones
+        # Obtener la lista de nombres de barrio desde la colección Estaciones
         barrios = db.Estaciones.distinct("Nom_barri")
         return render_template('formulario.html', barrios=barrios)
 
